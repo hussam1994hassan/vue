0bd2bb4d0b31bc3b8bc96ed175ed19b61b32f49c
ROOT <hussamhassan2014@gmail.com>
2023-03-27 06:23:45 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-<<<<<<< HEAD
   publicPath: "/vue/",
-});
-=======
-  publicPath: "/vue/"
 })
->>>>>>> 09a6d3a713649ecd5b4b52f84c7bfe281e1e7530
