69f61b2417543682e32d7f88a525cb79f21e9f6d
ROOT <hussamhassan2014@gmail.com>
2023-03-27 06:53:51 +0200

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -9,14 +9,13 @@ import ProjectPage from "./ProjectPage.vue";
 
 const routes = [
   {
-    path: "/vue/",
-    redirect: "/vue/dashboard",
+    path: "/vue",
     component: AppLayout,
     children: [
-      { path: "/vue/dashboard", name: "Dashboard", component: DashboardPage },
-      { path: "/vue/team", name: "Team", component: TeamPage },
-      { path: "/vue/calender", name: "Calender", component: CalenderPage },
-      { path: "/vue/project", name: "Project", component: ProjectPage },
+      { path: "/dashboard", name: "Dashboard", component: DashboardPage },
+      { path: "/team", name: "Team", component: TeamPage },
+      { path: "/calender", name: "Calender", component: CalenderPage },
+      { path: "/project", name: "Project", component: ProjectPage },
     ],
   },
 ];
